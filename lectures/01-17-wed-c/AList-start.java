8bb46adf35881ce1ff9a08b12a1c13565eb899f5
Joe Gibbs Politz <joe.politz@gmail.com>
2018-01-18 18:06:23 -0800

    .

---
+++ lectures/01-17-wed-c/AList-start.java
@@ -0,0 +1,16 @@
+
+/*
+  Which of these will start our implementation of ALists,
+  which implement the List interface?
+*/
+
+
+/*
+public class AList implements List<T> {
+public class AList implements List {
+public class AList<T> extends List<T> {
+public class AList<E> implements List<E> {
+public class AList extends List {
+
+}
+*/
---
+++ lectures/01-17-wed-c/AList.java
@@ -0,0 +1,11 @@
+
+/*
+  Which of these will start our implementation of ALists,
+  which implement the List interface?
+*/
+
+public class AList<E> implements List<E> {    // A
+
+
+}
+
